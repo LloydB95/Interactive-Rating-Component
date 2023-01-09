3e31451dbbe0eaa5c484d8134bccd3cdaef82788
Lloyd Buckle <lloyd.buckle@yahoo.co.uk>
2023-01-09 17:01:30 +0000

    added fonts and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     'index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'vdark-blue': 'hsl(216, 12%, 8%)',
+      },
+      fontFamily: {
+        'overpass': ['Overpass', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
